3cedbf53560fee7ba4c1c53e1e6e9051aefc5dad
jimmysday <jaminzhang99@gmail.com>
2025-04-11 15:28:41 -0700

    add note.swift

---
+++ temp1-moonsun/note.swift
@@ -0,0 +1,11 @@
+//Text("\(number)")
+//
+//Init array:
+//@State private images = Array(repeating: Array(repeating:"square",count:7),count:7)
+//
+//
+//Toggle(
+//       "Vibrate on Ring",
+//       systemImage: "dot.radiowaves.left.and.right",
+//       isOn: $vibrateOnRing
+//   )
